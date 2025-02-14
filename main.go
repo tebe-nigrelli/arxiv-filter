e2d0a872f7e0d7b13eca7831d9e6c12ed845eaa5
tebe-nigrelli <102675740+tebe-nigrelli@users.noreply.github.com>
2025-02-14 09:17:47 +0100

    fix: remove unused tags

--- main.go
+++ main.go
@@ -21,18 +21,14 @@ func main() {
 
 	fmt.Println("\n\nFeed Description:")
 	fmt.Printf("Feed Title: %s\n", feed.Title)
-	fmt.Printf("Feed Description: %s\n", feed.Description)
-	fmt.Printf("Feed Link: %s\n", feed.Link)
 	fmt.Printf("Feed Updated: %s\n", feed.Updated)
-	fmt.Printf("Feed Published: %s\n", feed.Published)
 
 	fmt.Println("\n\nFeed Items:")
 
-	for _, value := range feed.Items[0:1] {
+	for _, value := range feed.Items[0:5] {
 		n := 100 // You can set n to any value you prefer
 		fmt.Printf("Title: %s\n", value.Title[:min(len(value.Title), n)])
 		fmt.Printf("Description: %s\n", value.Description[:min(len(value.Description), n)])
-		fmt.Printf("Content: %s\n", value.Content[:min(len(value.Content), n)])
 		fmt.Printf("Link: %s\n", value.Link[:min(len(value.Link), n)])
 		fmt.Printf("Links: %v\n", value.Links)
 		fmt.Printf("Updated: %s\n", value.Updated[:min(len(value.Updated), n)])
@@ -48,11 +44,10 @@ func main() {
 			fmt.Printf("Image: %s\n", value.Image.URL[:min(len(value.Image.URL), n)])
 		}
 		fmt.Printf("Categories: %v\n", value.Categories)
-		fmt.Printf("Enclosures: %v\n", value.Enclosures)
-		fmt.Printf("DublinCoreExt: %v\n", value.DublinCoreExt)
-		fmt.Printf("ITunesExt: %v\n", value.ITunesExt)
 		fmt.Printf("Extensions: %v\n", value.Extensions)
-		fmt.Printf("Custom: %v\n", value.Custom)
+		for key, val := range value.Custom {
+			fmt.Printf("Custom[%s]: %s\n", key, val[:min(len(val), n)])
+		}
 	}
 
 	fmt.Println("\n\n ")
